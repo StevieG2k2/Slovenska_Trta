1362e23ae89d574e9954a0d0e4ec9006290578d2
lukagrahor <lukagrahor@gmail.com>
2022-12-07 10:17:55 +0100

    Administrator

--- web/Data/DbInitalizer.cs
+++ web/Data/DbInitalizer.cs
@@ -1,4 +1,7 @@
 using web.Models;
+using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace web.Data
 {
@@ -299,6 +302,54 @@ namespace web.Data
             };
 
             context.Odkup.AddRange(odkup);
+
+            var roles = new IdentityRole[] {
+                new IdentityRole{Id="1", Name="Administrator"}
+            };
+
+            foreach(IdentityRole r in roles)
+            {
+                context.Roles.Add(r);
+            }
+
+            var user = new ApplicationUser
+            {
+                FirstName = "Janez",
+                LastName = "Selski",
+                City = "Ljubljana",
+                Email = "janezselski@gmail.com",
+                NormalizedEmail = "XXXX@EXAMPLE.COM",
+                UserName = "bob@example.com",
+                NormalizedUserName = "bob@example.com",
+                PhoneNumber = "+111111111111",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString("D")
+            };
+
+
+            if (!context.Users.Any(u => u.UserName == user.UserName))
+            {
+                var password = new PasswordHasher<ApplicationUser>();
+                var hashed = password.HashPassword(user,"Geslo123&");
+                user.PasswordHash = hashed;
+                context.Users.Add(user);
+                
+            }
+
+            context.SaveChanges();
+            
+
+            var UserRoles = new IdentityUserRole<string>[]
+            {
+                new IdentityUserRole<string>{RoleId = roles[0].Id, UserId=user.Id},
+            };
+
+            foreach (IdentityUserRole<string> r in UserRoles)
+            {
+                context.UserRoles.Add(r);
+            }
+
             context.SaveChanges();
         }
     }
